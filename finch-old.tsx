5acb2d143b668d941f161c62124a563a406b440d
Pratik Manandhar <59083817+pratikfuse@users.noreply.github.com>
2023-06-14 13:58:10 +0545

    upload code for new package

--- finch-old.tsx
+++ finch-old.tsx
@@ -1,79 +1,130 @@
 import { useEffect } from 'react';
 
+export type SuccessEvent = {
+  code: string;
+};
+
+export type ErrorEvent = {
+  errorMessage: string;
+};
+
+export type ConnectOptions = {
+  category: string | null;
+  clientId: string;
+  manual: boolean;
+  onSuccess: (e: SuccessEvent) => void;
+  onError: (e: ErrorEvent) => void;
+  onClose: () => void;
+  payrollProvider: string | null;
+  products: string[];
+  sandbox: boolean;
+  zIndex: number;
+};
+
+type OpenFn = (overrides?: Partial<Pick<ConnectOptions, 'products'>>) => void;
+
+interface FinchConnectPostMessage {
+  data: {
+    name: string;
+    code?: string;
+    error?: string;
+    closed?: boolean;
+  };
+  origin: string;
+}
+
 const BASE_FINCH_CONNECT_URI = 'https://connect.tryfinch.com';
 const DEFAULT_FINCH_REDIRECT_URI = 'https://tryfinch.com';
 const FINCH_CONNECT_IFRAME_ID = 'finch-connect-iframe';
 const FINCH_AUTH_MESSAGE_NAME = 'finch-auth-message';
 
-const noop = () => {};
+const constructAuthUrl = ({
+  clientId,
+  payrollProvider,
+  category,
+  products,
+  manual,
+  sandbox,
+}: Partial<ConnectOptions>) => {
+  const authUrl = new URL(`${BASE_FINCH_CONNECT_URI}/authorize`);
 
-// eslint-disable-next-line import/prefer-default-export
-export const useFinchConnect = (options = {}) => {
-  const {
-    clientId,
-    products = [],
-    mode = 'employer',
-    category,
-    manual = false,
-    payrollProvider = null,
-    sandbox = false,
-    onSuccess = noop,
-    onError = noop,
-    onClose = noop,
-    zIndex = 999,
-  } = options;
+  if (clientId) authUrl.searchParams.append('client_id', clientId);
+  if (payrollProvider) authUrl.searchParams.append('payroll_provider', payrollProvider);
+  if (category) authUrl.searchParams.append('category', category);
+  authUrl.searchParams.append('products', (products ?? []).join(' '));
+  authUrl.searchParams.append('app_type', 'spa');
+  authUrl.searchParams.append('redirect_uri', DEFAULT_FINCH_REDIRECT_URI);
+  authUrl.searchParams.append('mode', 'employer');
+  if (manual) authUrl.searchParams.append('manual', String(manual));
+  if (sandbox) authUrl.searchParams.append('sandbox', String(sandbox));
+  // replace with actual SDK version by rollup
+  authUrl.searchParams.append('sdk_version', 'react-SDK_VERSION');
 
-  const _constructAuthUrl = (clientId, products) => {
-    const authUrl = new URL(`${BASE_FINCH_CONNECT_URI}/authorize`);
+  return authUrl.href;
+};
 
-    if (clientId) authUrl.searchParams.append('client_id', clientId);
-    if (payrollProvider) authUrl.searchParams.append('payroll_provider', payrollProvider);
-    if (category) authUrl.searchParams.append('category', category);
-    authUrl.searchParams.append('products', products.join(' '));
-    authUrl.searchParams.append('app_type', 'spa');
-    authUrl.searchParams.append('redirect_uri', DEFAULT_FINCH_REDIRECT_URI);
-    authUrl.searchParams.append('mode', mode);
-    if (manual) authUrl.searchParams.append('manual', manual);
-    if (sandbox) authUrl.searchParams.append('sandbox', sandbox);
-    /* global SDK_VERSION */
-    if (SDK_VERSION) authUrl.searchParams.append('sdk_version', `react-${SDK_VERSION}`);
+const noop = () => {
+  // intentionally empty
+};
 
-    return authUrl.href;
+const DEFAULT_OPTIONS: Omit<ConnectOptions, 'clientId'> = {
+  category: null,
+  manual: false,
+  onSuccess: noop,
+  onError: noop,
+  onClose: noop,
+  payrollProvider: null,
+  products: [],
+  sandbox: false,
+  zIndex: 999,
+};
+
+export const useFinchConnect = (options: Partial<ConnectOptions>): { open: OpenFn } => {
+  if (!options.clientId) throw new Error('must specify clientId in options for useFinchConnect');
+
+  const combinedOptions: ConnectOptions = {
+    clientId: '',
+    ...DEFAULT_OPTIONS,
+    ...options,
   };
 
-  const open = () => {
-    if (document.getElementById(FINCH_CONNECT_IFRAME_ID)) {
-      return null;
-    }
+  const open: OpenFn = (overrides?: Partial<Pick<ConnectOptions, 'products'>>) => {
+    const openOptions: ConnectOptions = {
+      ...combinedOptions,
+      ...overrides,
+    };
 
-    const iframe = document.createElement('iframe');
-    iframe.src = _constructAuthUrl(clientId, products);
-    iframe.frameBorder = '0';
-    iframe.id = FINCH_CONNECT_IFRAME_ID;
-    iframe.style.position = 'fixed';
-    iframe.style.zIndex = zIndex.toString();
-    iframe.style.height = '100%';
-    iframe.style.width = '100%';
-    iframe.style.top = '0';
-    iframe.style.backgroundColor = 'none transparent';
-    iframe.style.border = 'none';
-    document.body.prepend(iframe);
-    document.body.style.overflow = 'hidden';
+    if (!document.getElementById(FINCH_CONNECT_IFRAME_ID)) {
+      const iframe = document.createElement('iframe');
+      iframe.src = constructAuthUrl(openOptions);
+      iframe.frameBorder = '0';
+      iframe.id = FINCH_CONNECT_IFRAME_ID;
+      iframe.style.position = 'fixed';
+      iframe.style.zIndex = openOptions.zIndex.toString();
+      iframe.style.height = '100%';
+      iframe.style.width = '100%';
+      iframe.style.top = '0';
+      iframe.style.backgroundColor = 'none transparent';
+      iframe.style.border = 'none';
+      document.body.prepend(iframe);
+      document.body.style.overflow = 'hidden';
+    }
   };
 
   const close = () => {
     const frameToRemove = document.getElementById(FINCH_CONNECT_IFRAME_ID);
     if (frameToRemove) {
-      frameToRemove.parentNode.removeChild(frameToRemove);
+      frameToRemove.parentNode?.removeChild(frameToRemove);
       document.body.style.overflow = 'inherit';
     }
   };
 
   useEffect(() => {
-    function handleFinchAuth(event) {
-      const handleFinchAuthSuccess = (code) => onSuccess({ code });
-      const handleFinchAuthError = (error) => onError({ errorMessage: error });
-      const handleFinchAuthClose = () => onClose();
+    function handleFinchAuth(event: FinchConnectPostMessage) {
+      const handleFinchAuthSuccess = (code: string) => combinedOptions.onSuccess({ code });
+      const handleFinchAuthError = (error: string) =>
+        combinedOptions.onError({ errorMessage: error });
+      const handleFinchAuthClose = () => combinedOptions.onClose();
 
       if (!event.data) return;
       if (event.data.name !== FINCH_AUTH_MESSAGE_NAME) return;
@@ -89,7 +140,7 @@ export const useFinchConnect = (options = {}) => {
 
     window.addEventListener('message', handleFinchAuth);
     return () => window.removeEventListener('message', handleFinchAuth);
-  }, [onClose, onError, onSuccess]);
+  }, [combinedOptions.onClose, combinedOptions.onError, combinedOptions.onSuccess]);
 
   return {
     open,
